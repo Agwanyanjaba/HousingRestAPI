379d2245352bfea9af9dad64670c6788c740509e
Wycliffe Ochieng <bonkewycliffe@yahoo.com>
2019-07-16 12:33:48 +0300

    Update to Test Files

--- src/main/java/com/wycliffe/producer/HousingApplication.java
+++ src/main/java/com/wycliffe/producer/HousingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class HousingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
+		SpringApplication.run(HousingApplication.class, args);
 	}
 
 }
